c9b9201bf3d18c5351867ba37b034b7c3afa923f
bkial <bkial@kazacube.com>
2016-08-03 14:20:56 +0100

    second commit

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -6,7 +6,7 @@ class Course(models.Model):
     _name="openacademy.course"
 
     name = fields.Char(required=True,string="Titre")
-    titre = fields.Char()
+    titre = fields.Char(string="tit")
     description = fields.Text(string="Description")
     resp_id = fields.Many2one(comodel_name="res.users",string="Responsable")
 
