31b0464ab413a00a5afafbb8d05cb11c3243c11f
bkial <bkial@kazacube.com>
2016-08-04 12:36:53 +0100

    adding worflow

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -10,6 +10,10 @@ class Course(models.Model):
     resp_id = fields.Many2one(comodel_name="res.users",string="Responsable")
     sessions = fields.One2many(comodel_name="openacademy.session", inverse_name="course_id", string="Sessions", required=False, )
 
+    _sql_constraints = [
 
+        ('name_desc_check', 'CHECK name != description', 'Le titre doit être différent de la description'),
+        ('name_unique', 'UNIQUE(name)', 'Le titre doit être unique'),
 
+    ]
 
--- openacademy/models/partner.py
+++ openacademy/models/partner.py
@@ -7,8 +7,7 @@ class Partner(models.Model):
     _inherit="res.partner"
 
     instructor = fields.Boolean(string="Instructeur")
-    session_ids = fields.Many2many(comodel_name="openacademy.session", relation="session_part_rel",
-                                   column1="session_id", column2="part_id", string="session")
+    session_ids = fields.Many2many(comodel_name="openacademy.session", relation="session_part_rel",column1="part_id", column2="session_id", string="session")
 
 
 
